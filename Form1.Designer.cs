98c54aaacd712ccf704b510cd78db1cd8f195c1c
Andre Polykanine <ap@oire.me>
2022-09-14 02:20:20 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows.Forms;
+
+namespace MenuStripMenuBar;
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.MenuBar = new MenuStrip();
+        this.SuspendLayout();
+        // 
+        // MenuBar
+        // 
+        this.MenuBar.Location = new System.Drawing.Point(0, 0);
+        this.MenuBar.Name = "menuBar";
+        this.MenuBar.Size = new System.Drawing.Size(800, 28);
+        this.MenuBar.TabIndex = 0;
+        this.MenuBar.Text = "Main Menu";
+        this.MenuBar.AccessibleRole = AccessibleRole.MenuBar;
+        this.MenuFile = new ToolStripMenuItem("&File");
+        this.MenuFileNew = new ToolStripMenuItem();
+        this.MenuFileNew.Text = "&New...";
+        this.MenuFileNew.ShortcutKeys = Keys.Control | Keys.N;
+        this.MenuFileOpen = new ToolStripMenuItem();
+        this.MenuFileOpen.Text = "&Open...";
+        this.MenuFileOpen.ShortcutKeys = Keys.Control | Keys.O;
+        this.MenuFileSave = new ToolStripMenuItem();
+        this.MenuFileSave.Text = "&Save";
+        this.MenuFileSave.ShortcutKeys = Keys.Control | Keys.S;
+        this.MenuFileSaveAs = new ToolStripMenuItem();
+        this.MenuFileSaveAs.Text = "Save &As...";
+        this.MenuFileSaveAs.ShortcutKeys = Keys.Control | Keys.Shift | Keys.S;
+        this.MenuFileExit = new ToolStripMenuItem();
+        this.MenuFileExit.Text = "E&xit";
+        this.MenuFileExit.ShortcutKeys = Keys.Control | Keys.Q;
+        this.MenuFileItems = new ToolStripMenuItem[] { this.MenuFileNew, this.MenuFileOpen, this.MenuFileSave, this.MenuFileSaveAs, this.MenuFileExit };
+        this.MenuFile.DropDownItems.AddRange(this.MenuFileItems);
+        this.MenuEdit = new ToolStripMenuItem("&Edit");
+        this.MenuEditUndo = new ToolStripMenuItem();
+        this.MenuEditUndo.Text = "&Undo";
+        this.MenuEditUndo.ShortcutKeys = Keys.Control | Keys.Z;
+        this.MenuEditCut = new ToolStripMenuItem();
+        this.MenuEditCut.Text = "Cu&t";
+        this.MenuEditCut.ShortcutKeys = Keys.Control | Keys.X;
+        this.MenuEditCopy = new ToolStripMenuItem();
+        this.MenuEditCopy.Text = "&Copy";
+        this.MenuEditCopy.ShortcutKeys = Keys.Control | Keys.C;
+        this.MenuEditPaste = new ToolStripMenuItem();
+        this.MenuEditPaste.Text = "&Paste";
+        this.MenuEditPaste.ShortcutKeys = Keys.Control | Keys.V;
+        this.MenuEditItems = new ToolStripMenuItem[] { this.MenuEditUndo, this.MenuEditCut, this.MenuEditCopy, this.MenuEditPaste };
+        this.MenuEdit.DropDownItems.AddRange(this.MenuEditItems);
+        this.MenuView = new ToolStripMenuItem("&View");
+        this.MenuViewFullScreen = new ToolStripMenuItem();
+        this.MenuViewFullScreen.Text = "&Full Screen";
+        this.MenuViewFullScreen.ShortcutKeys = Keys.F11;
+        this.MenuViewFullScreen.Checked = false;
+        this.MenuViewFullScreen.CheckOnClick = true;
+        this.MenuViewShowStatusBar = new ToolStripMenuItem();
+        this.MenuViewShowStatusBar.Text = "Show &Status bar";
+        this.MenuViewShowStatusBar.Checked = true;
+        this.MenuViewShowStatusBar.CheckOnClick = true;
+        this.MenuViewItems = new ToolStripMenuItem[] { this.MenuViewFullScreen, this.MenuViewShowStatusBar };
+        this.MenuView.DropDownItems.AddRange(this.MenuViewItems);
+        this.MenuHelp = new ToolStripMenuItem("&Help");
+        this.MenuHelpUserManual = new ToolStripMenuItem();
+        this.MenuHelpUserManual.Text = "User &Manual";
+        this.MenuHelpUserManual.ShortcutKeys = Keys.F1;
+        this.MenuHelpAbout = new ToolStripMenuItem("&About...");
+        this.MenuHelpItems = new ToolStripMenuItem[] { this.MenuHelpUserManual, this.MenuHelpAbout };
+        this.MenuHelp.DropDownItems.AddRange(this.MenuHelpItems);
+        this.MenuTopLevelItems = new ToolStripMenuItem[] { this.MenuFile, this.MenuEdit, this.MenuView, this.MenuHelp };
+        this.MenuBar.Items.AddRange(this.MenuTopLevelItems);
+        // 
+        // Form1
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.AccessibleRole = AccessibleRole.Application;
+        this.Controls.Add(this.MenuBar);
+        this.MainMenuStrip = this.MenuBar;
+        this.Name = "Form1";
+        this.Text = "Newer Menu Bar Example";
+        this.Focus();
+        this.ResumeLayout(false);
+        this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private MenuStrip MenuBar;
+    private ToolStripMenuItem MenuFile;
+    private ToolStripMenuItem MenuFileNew;
+    private ToolStripMenuItem MenuFileOpen;
+    private ToolStripMenuItem MenuFileSave;
+    private ToolStripMenuItem MenuFileSaveAs;
+    private ToolStripMenuItem MenuFileExit;
+    private ToolStripMenuItem[] MenuFileItems;
+    private ToolStripMenuItem MenuEdit;
+    private ToolStripMenuItem MenuEditUndo;
+    private ToolStripMenuItem MenuEditCut;
+    private ToolStripMenuItem MenuEditCopy;
+    private ToolStripMenuItem MenuEditPaste;
+    private ToolStripMenuItem[] MenuEditItems;
+    private ToolStripMenuItem MenuView;
+    private ToolStripMenuItem MenuViewFullScreen;
+    private ToolStripMenuItem MenuViewShowStatusBar;
+    private ToolStripMenuItem[] MenuViewItems;
+    private ToolStripMenuItem MenuHelp;
+    private ToolStripMenuItem MenuHelpUserManual;
+    private ToolStripMenuItem MenuHelpAbout;
+    private ToolStripMenuItem[] MenuHelpItems;
+    private ToolStripMenuItem[] MenuTopLevelItems;
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # menustrip-example
 A simple MenuStrip example for showing WindowsForms accessibility degradation.
+
+## The Story
+Before, in .Net Framework, there was a classic [MainMenu](https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.mainmenu?view=netframework-4.8) control that provided a classic native menu bar. It was super accessible, all screen readers read it correctly, the *Escape* key was processed automatically, as was the *Alt* key (it toggled the focus between the menu bar and the main window). to put it short, it was analogous to a menu bar written in pure Windows API.
+
+However now, in .Net 6, there is only this [MenuStrip](https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.menustrip?view=windowsdesktop-6.0) class which behaves differently:
+* JAWS for Windows reports each item of the horizontal menu bar as having a submenu. Each vertical dropdown menu, when entered, is reported as context menu which is wrong. NVDA reports those items as collapsed, which is also wrong, they are just menu bar items. Narrator is super verbose in any menu bar, so I would suggest to try the other two.
+* The *Escape* key seems to have effect but the screen readers do not report "leaving menus" and the menu status seem to persist. At least, JAWS reports "leaving menus" only when alt-tabbing out of the sample application. The *Alt* key seems to have no effect.
+* The checked status of a checkable menu item is reported only by Narrator. As it is reported correctly in native menu bars, the issue is in the custom control itself.
+* The menu bar implemented this way lacks the system menu with elements like Move, Size, Minimize, Maximize etc. Alt+Space also does not work. With the native menu bar this menu is given free of charge.
